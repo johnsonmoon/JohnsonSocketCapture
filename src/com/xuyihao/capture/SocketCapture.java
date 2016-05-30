7d2831ce3fc8e4d7d832ac1ce860ec7bf52fcd03
johnsonmoon <841846248@qq.com>
2016-05-30 12:39:36 +0800

    First commit.

---
+++ src/com/xuyihao/capture/SocketCapture.java
@@ -0,0 +1,10 @@
+package com.xuyihao.capture;
+
+/**
+ * created by xuyihao on 2016/5/30
+ * @author Johnson
+ * @description Socket 抓包类
+ * */
+public class SocketCapture {
+
+}
---
+++ src/com/xuyihao/test/JohnsonMain.java
@@ -0,0 +1,14 @@
+package com.xuyihao.test;
+
+/**
+ * created by xuyihao on 2016/5/30
+ * @author Johnson
+ * @description 测试类，包含主方法
+ * */
+public class JohnsonMain {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
