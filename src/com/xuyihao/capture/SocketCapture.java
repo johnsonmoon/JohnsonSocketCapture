7a4e4295109b92d5ccc0e2fa40fda1f31beeed6e
johnsonmoon <841846248@qq.com>
2016-05-30 23:18:15 +0800

    Second commit.

--- src/com/xuyihao/capture/SocketCapture.java
+++ src/com/xuyihao/capture/SocketCapture.java
@@ -1,10 +1,40 @@
 package com.xuyihao.capture;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+
 /**
  * created by xuyihao on 2016/5/30
  * @author Johnson
- * @description Socket 抓包类
+ * @description Socket 抓包类                   
  * */
 public class SocketCapture {
 
+	/**
+	 * fields
+	 * */
+	private ServerSocket serverSocket;
+	
+	/**
+	 * constructor                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
+	 * */
+	public SocketCapture(){
+		
+	}
+	
+	/**
+	 * constructor2
+	 * @param portNumber 端口号
+	 * */
+	public SocketCapture(int portNumber){
+		try{
+			this.serverSocket = new ServerSocket(portNumber);
+		}catch(IOException e){
+			e.printStackTrace();
+			System.out.println("Socket创建失败!");
+		}
+	}
+	
+	
+		
 }
